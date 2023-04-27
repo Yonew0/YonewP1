4f8a7398b49a70c0a6cba8a19c426ba1402089ea
Yonew0 <131365746+Yonew0@users.noreply.github.com>
2023-04-27 18:44:51 +0700

    my kidkod projects
    i so stupid to make 2 and 4 exs(((

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
-print("Hello World")
+abc = "abcdefghijklmnopqrstuvwxyz"
+def lower_total(w) :
+    c = 0;
+    for cha in w :
+        for d in abc.lower() :
+            if d == cha :
+                c += 1
+    return c
+
+def upper_total(w) :
+    c = 0
+    for cha in w :
+        for d in abc.upper() :
+            if d == cha :
+                c += 1
+    return c
+
+nadpiss = "Yonew Sjel kakashky"
+print(f'{nadpiss} {lower_total(nadpiss)}')
+print(f'{nadpiss} {upper_total(nadpiss)}')
 input()
-print("ijeidjedijeidejidjiejdiwejdiwehifuehyegwhey4r")
---
+++ main2.py
@@ -0,0 +1,5 @@
+def pir(x) :
+    for i in range(x+1) :
+        print(str(i) * i)
+a = int(input("int : "))
+pir(a)
